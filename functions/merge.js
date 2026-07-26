239b2e6eaab76e0092bc148866b9ea8c5e319ead
manu <bhuvansinghjodha@proton.me>
2026-07-26 13:50:56 +0530

    feat: add Font Awesome icons and update file list styles
    - Add Font Awesome 4.7.0 library assets (CSS and documentation)
    - Add `.total_files` and `.files` classes to `assets.css` to style the file list container and individual file items

--- functions/merge.js
+++ functions/merge.js
@@ -1,8 +1,11 @@
 // Merge button variables
 const mergebtn = document.querySelector("#merge-btn");
 const mergeDialog = document.querySelector("#mergeDialogbox");
+
 const mergeInput = document.querySelector("#merge-files");
+
 const submitMerge = document.querySelector("#merge-submit");
+const filesContainer = document.querySelector(".total_files");
 
 const uploadMerge = document.querySelector("#uploadfilesmerge");
 
@@ -18,4 +21,47 @@ mergebtn.addEventListener("click", () => {
 
 uploadMerge.addEventListener("click", () => {
   mergeInput.click();
+});
+
+mergeInput.addEventListener("change", (event) => {
+  event.preventDefault();
+
+  const files = event.target.files;
+
+  for (let i = 0; i < files.length; i++) {
+    const box = document.createElement("button");
+
+    box.className = "files";
+    box.innerHTML = `<i class="fa fa-file" aria-hidden="true"></i>`;
+    box.dataset.index = i;
+    filesContainer.appendChild(box);
+  }
+});
+
+filesContainer.addEventListener("click", (e) => {
+  const clicked = e.target.closest(".files");
+
+  if (clicked) {
+    const removeIndex = parseInt(clicked.dataset.index, 10);
+
+    const dt = new DataTransfer();
+
+    const currentFile = mergeInput.files;
+
+    for (let i = 0; i < currentFile.length; i++) {
+      if (i !== removeIndex) {
+        dt.items.add(currentFile[i]);
+      }
+    }
+
+    mergeInput.files = dt.files;
+
+    clicked.remove();
+
+    // resyncing the remaining boxes
+    const remainingBoxes = filesContainer.querySelectorAll(".files");
+    remainingBoxes.forEach((box, index) => {
+      box.dataset.index = index;
+    });
+  }
 });
--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('hello world')
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+def root():
+    pass
